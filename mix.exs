081ed49772bf77b11420ac905513f264e2a429ca
James Harton <james@rabidtech.co.nz>
2016-01-22 16:17:26 +1300

    Update mix.exs for publication.

--- mix.exs
+++ mix.exs
@@ -8,11 +8,23 @@ defmodule Decks.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      preferred_cli_env: [espec: :test],
+     maintainers: [ "James Harton <james@messagerocket.co>" ],
+     licenses: ["MIT"],
+     links: %{
+       "GitHub" => "https://github.com/elixir_decks"
+     },
+     description: description,
      deps: deps]
   end
 
   def application do
     [applications: [:logger]]
+  end
+
+  def description do
+    """
+    Implements standard card decks for Elixir-based card games.
+    """
   end
 
   defp deps do
